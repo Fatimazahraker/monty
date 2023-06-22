97ece8db7d2f293148507f023027f35b02e00edf
Red-Rim <salmaouakhir20@gmail.com>
2023-06-22 12:46:57 -0700

    .

--- monty.h
+++ monty.h
@@ -64,5 +64,6 @@ int is_number(char *string);
 void free_stack(stack_t *head);
 void pop(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
 #endif
 
--- opcodes.c
+++ opcodes.c
@@ -41,4 +41,35 @@ void pint(stack_t **stack, unsigned int line_number)
 
     printf("%d\n", (*stack)->n);
 }
+/*---------------------swap-------------------------*/
+/**
+ * swap - swaps the top two elements of the stack
+ * @stack: stack
+ * @line_number: line numb
+ * Return: void
+ */
+void swap(stack_t **stack, unsigned int line_number)
+{
+    stack_t *top1, *top2;
+
+    if (!stack || !*stack || !(*stack)->n)
+    {
+        fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    top1 = *stack;
+    top2 = (*stack)->n;
+
+    top1->prev = top2;
+    top1->n = top2->n;
+
+    if (top2->n)
+        top2->n->prev = top1;
+
+    top2->prev = NULL;
+    top2->n = top1;
+
+    *stack = top2;
+}
 
