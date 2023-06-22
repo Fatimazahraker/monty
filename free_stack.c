e3f06285a8b8f1090b645ddeebe9f8606eaf9792
Red-Rim <salmaouakhir20@gmail.com>
2023-06-21 18:35:43 -0700

    .

--- free_stack.c
+++ free_stack.c
@@ -2,18 +2,17 @@
 
 /**
  * free_stack - frees dlistint_t
- * @stack: head og the list
- *
+ * @stack: head
  * Return: void
  */
-void free_stack(stack_t *stack)
+void free_stack(stack_t *head)
 {
-	stack_t *current = stack;
+	stack_t *current = head;
 	stack_t *next;
 
-	if (stack)
+	if (head)
 	{
-		next = stack->next;
+		next = head->next;
 		while (current)
 		{
 			free(current);
