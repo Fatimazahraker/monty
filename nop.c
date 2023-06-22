2f1be5bf00f6405eb7d0187db7e795bb4a8a002e
Red-Rim <salmaouakhir20@gmail.com>
2023-06-21 19:24:04 -0700

    .

---
+++ nop.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+ * nop - does nothing
+ * @stack: head
+ * @line_number: line numb
+ *
+ * Return: void
+ */
+void nop(stack_t **head, unsigned int line_number)
+{
+	(void)head;
+	(void)line_number;
+	/* Do nothing */
+}
